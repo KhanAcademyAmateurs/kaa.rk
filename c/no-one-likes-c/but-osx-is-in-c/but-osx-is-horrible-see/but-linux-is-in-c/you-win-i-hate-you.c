ff18f3cc8121946456f1580a4237f9334fde11df
Raumaan Kidwai <raumaankidwai@gmail.com>
2016-01-12 18:24:03 -0500

    Create you-win-i-hate-you.c

---
+++ c/no-one-likes-c/but-osx-is-in-c/but-osx-is-horrible-see/but-linux-is-in-c/you-win-i-hate-you.c
@@ -0,0 +1,38 @@
+/**       but
+stil      l,n
+o- on     e l
+ik  es    C.B
+ec   us   thi
+s.    */  int
+f(     int b)
+
+{return b>2?f
+(b
+-1
+)+f(b-2):1;}\
+m\
+a\
+in(){putchar(
+
+f(        11)
+ +11     -11
+  /11   );\
+   printf(
+    "%d",
+
+(11)+11-11/11
+);
+p\
+uts(" iz bad\
+.\
+ \
+i aint gonna"
+
+"use it, not\
+ n"        "\
+e"         "\
+ver");return\
+f(      1)
+;}       /*
+ne        ve
+r.         */
