d7411bf477cd70d7bec47752378f55a3a5dd34fd
Raumaan Kidwai <raumaankidwai@gmail.com>
2016-01-23 07:39:44 -0500

    Update poly.py

--- poly/poly.py
+++ poly/poly.py
@@ -1,15 +1,13 @@
-#B"This is a Pyth program!"#
+#"This is a Pyth program!"
 print"This is a Python program!"
 
 -----
 Explanation (Pyth):
   First line:
     #             Start infinite while loop (on error, cancels it and breaks)
-      B           Break
     "..."         Print string
-    #             Start infinite while loop (on error, cancels it and breaks)
   Second line:
     print"..."    Invalid Pyth code. Causes the infinite while loop to cancel the error and break.
 Explanation (Python):
-  #B"..."#        Python comment
+  #"..."          Python comment
   print"..."      Print string
